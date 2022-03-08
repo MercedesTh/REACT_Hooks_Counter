d869eb2496c8ef6374c2bec3d242736f77454d43
Mercedes <mercedeswagner84@gmail.com>
2022-03-08 09:23:21 +0100

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+
 function App() {
+  const [count, setCount] = useState(0)
+
+  function increment() {
+    setCount(count + 1)
+  }
+
+  function decrement() {
+    setCount(count - 1)
+  }
+
+  function refresh() {
+    window.location.reload();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter {count}</h1>
+      <a onClick={increment}>+</a>
+      <a onClick={decrement}>-</a>
+      <a onClick={refresh}>Reset</a>
+
     </div>
   );
 }
